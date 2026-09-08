1f413369264e574e3dd190cfa86efa2a3ce136d2
Alan <alansauga5904@gmail.com>
2026-09-07 18:58:14 -0600

    Estilos con bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
             todossection.style.display = 'block';
             gruposection.style.display = 'none';
             sinleersection.style.display = 'none';
+
+            todosbtn.classList.remove('btn-outline-success');
+            todosbtn.classList.add('btn-success');
+
+            grupobtn.classList.add('btn-outline-success');
+            grupobtn.classList.remove('btn-success');
+
+            sinleerbtn.classList.add('btn-outline-success');
+            sinleerbtn.classList.remove('btn-success');
         }
     });
     
@@ -21,6 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
             todossection.style.display = 'none';
             gruposection.style.display = 'block';
             sinleersection.style.display = 'none';
+
+            grupobtn.classList.remove('btn-outline-success');
+            grupobtn.classList.add('btn-success');
+
+            todosbtn.classList.add('btn-outline-success');
+            todosbtn.classList.remove('btn-success');
+
+            sinleerbtn.classList.add('btn-outline-success');
+            sinleerbtn.classList.remove('btn-success');
         }
     });
     
@@ -29,6 +47,15 @@ document.addEventListener('DOMContentLoaded', () => {
             todossection.style.display = 'none';
             gruposection.style.display = 'none';
             sinleersection.style.display = 'block';
+
+            sinleerbtn.classList.remove('btn-outline-success');
+            sinleerbtn.classList.add('btn-success');
+
+            todosbtn.classList.add('btn-outline-success');
+            todosbtn.classList.remove('btn-success');
+
+            grupobtn.classList.add('btn-outline-success');
+            grupobtn.classList.remove('btn-success');
         }
     });
 });
